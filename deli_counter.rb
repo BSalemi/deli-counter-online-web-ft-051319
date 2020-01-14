7b332acd5d4d4a93878c07fb84c8f9a588a51bc6
Brian Salemi <briandsalemi@gmail.com>
2020-01-14 14:17:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+require 'pry'
+
+katz_deli = []
+
+def line(deli)
+  if deli.length > 0
+    current_line = "The line is currently:"
+    deli.each_with_index do |customer, index|
+      current_line << " #{index + 1}. #{customer}"
+    end
+     puts current_line
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(deli, customer)
+    deli.push(customer)
+    puts "Welcome, #{customer}. You are number #{deli.length} in line."
+end
+
+def now_serving(deli)
+  if deli.empty?
+    puts "The line is empty."
+end
